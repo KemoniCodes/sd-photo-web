3e241a30eb7fe8d9da296fa907597ea5c1616f14
kemonicodes22 <kemoniw333@gmail.com>
2023-11-07 02:05:19 -0800

    sanity/next init

---
+++ sanity.config.ts
@@ -0,0 +1,20 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'sd-photo-web',
+
+  projectId: '00nf1c6w',
+  dataset: 'production',
+
+  basePath: '/studio',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
