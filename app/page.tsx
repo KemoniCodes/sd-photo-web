8537e926b42a390df67f7f27f47ef5958e842120
kemonicodes22 <kemoniw333@gmail.com>
2023-12-15 15:54:23 -0800

    clean up

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,5 @@ export default async function HomePage() {
     <>
       <Home />
     </>
-    // <main className="max-w-7xl mx-auto lg:px-16 px-6">
-    //   <section className="flex xl:flex-row flex-col xl:items-center items-start xl:justify-center justify-between gap-x-12 lg:mt-32 mt-20 mb-16">
-    //     {/* {collection &&
-    //       collection.map((data) => (
-    //       <></>
-    //       ))} */}
-    //   </section>
-    // </main>
   );
 }
