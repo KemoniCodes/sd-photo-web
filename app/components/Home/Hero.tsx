0d7f97ecca36dcca32e385238c4dd66ba025e361
kemonicodes22 <kemoniw333@gmail.com>
2023-12-21 21:55:49 -0800

    prod mobile fix

--- app/components/Home/Hero.tsx
+++ app/components/Home/Hero.tsx
@@ -43,7 +43,7 @@ export default function Hero() {
           width="100%"
           height="100vh"
           playsInline
-          // autoPlay
+          autoPlay
           id="autoplayVideo"
           muted
         >
