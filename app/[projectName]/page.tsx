239c9d5e6eb674c098eb7a30fff2b9f135f7f00a
kemonicodes22 <kemoniw333@gmail.com>
2024-03-04 18:30:40 -0800

    mobile

--- app/[projectName]/page.tsx
+++ app/[projectName]/page.tsx
@@ -31,7 +31,6 @@ function Project({ collectionTitle, _id, slug, hoverColor, mainImage, images }: 
     const animation = gsap.to(container, {
       x: () => -containerWidth,
       scrollTrigger: {
-        markers: true,
         trigger: '.scrollerWrapper',
         start: 'top top',
         end: () => `+=${containerWidth}`,
@@ -58,20 +57,19 @@ function Project({ collectionTitle, _id, slug, hoverColor, mainImage, images }: 
   return (
     <>
       <div className="project flex">
-        <div className="projectName w-[30%] fixed h-screen text-left">
-          <h3 className='break-words'>{collectionTitle}</h3>
+        <div className="projectName w-full lg:w-[30%] relative lg:fixed h-screen text-left">
+          <h3 className='break-words w-[90vw] lg:w-auto'>{collectionTitle}</h3>
           <h3 className='absolute bottom-0 pb-20 cursor-pointer' onClick={handleBackClick}>back</h3>
         </div>
 
-
-        <div className="projectImages projectImagesContainer absolute left-[35%] pb-32 ">
+        <div className="projectImages projectImagesContainer absolute left-0 mt-40 lg:mt-0 lg:left-[35%] pb-32 ">
 
           {/* the div wrapping the image slider only */}
-          <div className="scrollerContainer w-full overflow-scroll"
+          <div className="scrollerContainer w-full overflow-scroll hidden lg:block"
           // style={{ height: 'calc(100vh + 210vw)' }}
           >
             <div className="scrollerWrapper overflow-scroll">
-              <div className="imagesSlider w-full flex gap-2 " ref={containerRef}
+              <div className="imagesSlider w-full lg:flex hidden gap-2" ref={containerRef}
               // style={{ height: 'calc(100vh + 210vw)' }}
               >
                 {mainImage && (
@@ -91,8 +89,13 @@ function Project({ collectionTitle, _id, slug, hoverColor, mainImage, images }: 
               </div>
             </div>
           </div>
-          <div className="otherImages flex flex-wrap gap-x-2 gap-y-32 justify-center mt-32">
-            {images?.slice(3).map((img, index) => (
+          <div className="otherImages flex flex-wrap gap-x-2 gap-y-4 lg:gap-y-32 justify-center mt-32">
+          {mainImage && (
+                  <Image
+                    className='object-cover block lg:hidden' src={`${mainImage}`} height={600} width={394} alt={mainImage.alt} priority={true}
+                  />
+                )}
+            {images?.map((img, index) => (
               <Image key={index} className='object-cover' src={`${img}`} height={600} width={394} alt={img.alt} priority={true} />
             ))}
           </div>
