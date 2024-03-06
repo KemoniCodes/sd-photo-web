745bd43a84d7bb6aef9c5875f89abac608ad18c6
kemonicodes22 <kemoniw333@gmail.com>
2024-03-05 21:01:45 -0800

    prod check

--- app/components/Home/Hero.tsx
+++ app/components/Home/Hero.tsx
@@ -1,18 +1,25 @@
-import React, { useEffect, useState } from "react"
-import { getHomepageVideo } from "@/sanity/sanity.query"
-import { HomeType } from "@/types"
-import LoadingScreen from "./loadingScreen"
+import React, { useEffect, useState } from "react";
+import { getHomepageVideo } from "@/sanity/sanity.query";
+import { HomeType } from "@/types";
+import { gsap } from "gsap";
+import { SplitText } from "gsap-trial/SplitText";
+import LoadingScreen from "./loadingScreen";
 
+gsap.registerPlugin(SplitText);
 
 export default function Hero() {
-  const [hpVideo, sethpVideo] = useState<HomeType | null>(null)
+  const [hpVideo, setHpVideo] = useState<HomeType | null>(null);
+
   useEffect(() => {
     async function fetchData() {
-      const data = await getHomepageVideo();
-      sethpVideo(data);
+      try {
+        const data = await getHomepageVideo();
+        setHpVideo(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     }
     fetchData();
-    console.log(hpVideo)
 
     const videoElement = document.getElementById("autoplayVideo") as HTMLVideoElement | null;
     if (videoElement) {
@@ -20,27 +27,45 @@ export default function Hero() {
         console.error("Autoplay was prevented:", error);
       });
     }
-  }, [hpVideo])
+
+    // const split = new SplitText(".line", { type: "chars" });
+    // gsap.from(split.chars, {
+    //   duration: 1,
+    //   y: 100,
+    //   autoAlpha: 0,
+    //   stagger: .035
+    // });
+
+
+    gsap.utils.toArray(".line").forEach((element) => {
+      const split = new SplitText(element, { type: "chars" });
+      gsap.from(split.chars, {
+        duration: 1,
+        y: 100,
+        autoAlpha: 0,
+        stagger: 0.035,
+        ease: "power4.out",
+      });
+    });
+
+  }, []);
+
 
   if (!hpVideo) {
-    return <LoadingScreen/>
-    // return <p>Loading...</p>;
+    return <LoadingScreen />;
   }
 
   return (
     <>
-      <style global jsx>{`
+      <style jsx>{`
         body {
-          // margin: 0;
-          // margin-top: 1.5rem;
-          // margin-left: 1rem;
           padding: 0;
           overflow-y: hidden;
         }
       `}</style>
       <div className="-m-[1.5rem] lg:hidden block">
         <video
-          className=" h-[110vh] object-cover"
+          className="h-[110vh] object-cover"
           preload="auto"
           width="100%"
           height="100vh"
@@ -54,10 +79,9 @@ export default function Hero() {
       </div>
 
       <div className="nameDiv absolute lg:bottom-0 -bottom-[.7rem] lg:pl-0 pl-[.7rem]">
-        <h1 className="lg:-ml-2 -ml-[.8rem]">sam</h1>
-        <h1 className="-ml-4">dameshek</h1>
+        <h1 className="line overflow-hidden lg:-ml-2 -ml-[.8rem]">sam</h1>
+        <h1 className="line overflow-hidden -ml-4">dameshek</h1>
       </div>
-
     </>
-  )
-};
+  );
+}
